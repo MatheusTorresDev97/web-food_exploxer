fb139910ec41f2dca40681666d8fd6e402ade9d7
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-29 11:33:01 -0300

    add global styles and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div>App</div>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
