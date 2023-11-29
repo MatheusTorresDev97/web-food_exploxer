8ccda795c4a8fc3b8d56ed8a34cae04105ece90b
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-29 12:26:51 -0300

    feat/ create responsivity of the SingIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Form } from "./styles";
+import { Container, Form, Brand } from "./styles";
 import Input from "../../components/Input";
 import ButtonText from "../../components/ButtonText";
 import Logo from "../../assets/logo.svg";
@@ -6,10 +6,13 @@ import Logo from "../../assets/logo.svg";
 const SignIn = () => {
   return (
     <Container>
+      <Brand>
+        <img src={Logo} alt=" Logo do food explorer" />
+        <h1>food explorer</h1>
+      </Brand>
       <Form>
         <div className="top">
           <img src={Logo} alt=" Logo do food explorer" />
-          <h1>Food Explorer</h1>
         </div>
         <h2>Faça seu login:</h2>
         <Input title="Nome:" type="text" placeholder="exemplo@exemplo.com.br" />
@@ -18,7 +21,7 @@ const SignIn = () => {
           type="password"
           placeholder="No mínimo 6 caracteres"
         />
-        <ButtonText  title="Criar uma conta" />
+        <ButtonText title="Criar uma conta" />
       </Form>
     </Container>
   );
