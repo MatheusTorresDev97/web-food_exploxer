528c783edd7c05e6d49a94186cb4afad90fdb33b
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-05 08:12:17 -0300

    refactor/ improves the animation of the 'Loading'

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,10 +3,11 @@ import { Container } from "./styles";
 const Loading = () => {
   return (
     <Container>
-        <div className="circle"></div>
-        <p>Carregando...</p>
+      <div className="circle"></div>
+      <p className="loading">Carregando...</p>
+      <p className="just-moment">Só mais um momento...</p>
     </Container>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
