3783deee350a1674b4277c56934ddae23e55fbcb
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-05 10:42:26 -0300

    feat/ adds the function of logging

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,28 +1,30 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { useContext, createContext } from "react";
+import { useContext, createContext, useState } from "react";
+import { api } from "../services/api";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-    let userInfos = {
-        name: "Matheus",
-        email: "matheus@email.com",
-        isAdm: true,
-    };
+  const [userInfos, setUserInfos] = useState(null);
 
-    userInfos = null;
+  function authenticateUser({ user, token }) {
+    setUserInfos(user);
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-    return (
-        <AuthContext.Provider value={{ userInfos }}>
-            {children}
-        </AuthContext.Provider>
-    );
+    localStorage.setItem("@food_explorer:user", JSON.stringify(user));
+    localStorage.setItem("@food_explorer:token", token);
+  }
+
+  return (
+    <AuthContext.Provider value={{ userInfos, authenticateUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export function useAuth() {
-    const context = useContext(AuthContext);
+  const context = useContext(AuthContext);
 
-    return context;
+  return context;
 }
-
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,8 +3,55 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 import ButtonText from "../../components/ButtonText";
 import Logo from "../../assets/logo.svg";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { validateData } from "../../utils/dataValidator";
+import { useRequest } from "../../hooks/useRequest";
+import { useAuth } from "../../hooks/useAuth";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { manageRequests } = useRequest();
+  const navigate = useNavigate();
+  const { authenticateUser } = useAuth();
+
+  async function handleSignIn() {
+    const allInputIsValid = validateData({ email, password });
+
+    if (!allInputIsValid) {
+      return;
+    }
+
+    const response = await manageRequests("post", "sessions", {
+      email,
+      password,
+    });
+
+    if (response instanceof Error) {
+      return navigate("/off-air");
+    }
+
+    const theResultWasASuccess = Object.prototype.hasOwnProperty.call(response.data, "user");
+
+    if (!theResultWasASuccess) {
+      if (response.data.message) {
+        alert(response.data.message);
+      } else {
+        alert("Não foi possível logar. Por favor tente novamente mais tarde.");
+      }
+
+      return;
+    }
+
+    const { user, token } = response.data;
+    navigate("/");
+
+    authenticateUser({ user, token });
+  }
+
   return (
     <Container>
       <Brand>
@@ -16,13 +63,26 @@ const SignIn = () => {
           <img src={Logo} alt=" Logo do food explorer" />
         </div>
         <h2>Faça seu login:</h2>
-        <Input title="Nome:" type="text" placeholder="exemplo@exemplo.com.br" />
+        <Input
+          title="Nome:"
+          type="text"
+          placeholder="exemplo@exemplo.com.br"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <Input
           title="Senha:"
           type="password"
           placeholder="No mínimo 6 caracteres"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <Button title="Entrar" type="button" isHighlighted />
+        <Button
+          title="Entrar"
+          type="button"
+          isHighlighted
+          onClick={handleSignIn}
+        />
         <ButtonText title="Criar uma conta" to="/register" />
       </Form>
     </Container>
