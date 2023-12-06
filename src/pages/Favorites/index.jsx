86eb36d7165b5e1437188f2204a3b53d15f2ab2e
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-06 09:31:36 -0300

    feat/add logic to render the items in the favorite

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Container, Content } from "./styles";
 
@@ -7,101 +7,59 @@ import Header from "../../components/Header";
 import Wrapper from "../../components/Wrapper";
 import Card from "../../components/Card";
 
+import { useRequest } from "../../hooks/useRequest";
+
 const Favorites = () => {
-    const [favorites, setFavorites] = useState([
-        {
-            id: 2,
-            title: "Macarrão",
-            description: "Um prato italiano",
-            price: 32.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-            id: 7,
-            title: "Salada 2022",
-            description: "Um prato italiano",
-            price: 9.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-            id: 49,
-            title: "Macarrão 88",
-            description: "Um prato italiano",
-            price: 32.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-            id: 482,
-            title: "Macarrão",
-            description: "Um prato italiano",
-            price: 32.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-            id: 647,
-            title: "Salada 2022",
-            description: "Um prato italiano",
-            price: 9.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-            id: 549,
-            title: "Macarrão 88",
-            description: "Um prato italiano",
-            price: 32.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-            id: 452,
-            title: "Macarrão",
-            description: "Um prato italiano",
-            price: 32.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-        {
-            id: 57,
-            title: "Salada 2022",
-            description: "Um prato italiano",
-            price: 9.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        },
-    ]);
-    return (
-        <Container>
-            <Header />
-            <Wrapper>
-                <Content>
-                    <h1>Favoritos</h1>
-                    <section className="cards">
-                        {favorites &&
-                            favorites.map(favorite => {
-                                const { title, description, id, image, price } = favorite;
+  const [favorites, setFavorites] = useState();
 
-                                return (
-                                    <Card
-                                        key={String(id)}
-                                        image={image}
-                                        title={title}
-                                        description={description}
-                                        meal_id={id}
-                                        price={price}
-                                    />
-                                );
-                            })}
-                    </section>
-                </Content>
-            </Wrapper>
-            <Footer />
-        </Container>
-    )
-}
+  console.log({ favorites });
 
-export default Favorites
+  const { manageRequests } = useRequest();
+
+  useEffect(() => {
+    async function fetchFavorites() {
+      const response = await manageRequests("get", "/favorites");
+
+      setFavorites(response);
+    }
+
+    fetchFavorites();
+  }, [manageRequests]);
+  return (
+    <Container>
+      <Header />
+      <Wrapper>
+        <Content>
+          {!favorites || favorites.length == 0 ? (
+            <h1>Você ainda não tem favoritos</h1>
+          ) : (
+            <>
+              <h1>Favoritos</h1>
+              <section className="cards">
+                {favorites &&
+                  favorites.map((favorite) => {
+                    const { title, description, id, image, price } = favorite;
+
+                    return (
+                      <Card
+                        key={String(id)}
+                        image={image}
+                        title={title}
+                        description={description}
+                        meal_id={id}
+                        price={price}
+                        isFav={true}
+                      />
+                    );
+                  })}
+              </section>
+            </>
+          )}
+        </Content>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Favorites;
