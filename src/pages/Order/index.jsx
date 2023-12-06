914dc63de10c1a7e8f3b10c31758ae825140a576
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-06 12:18:01 -0300

    feat/ adds the logic of loading

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { useParams, useNavigate } from "react-router-dom";
 import Check from "../../assets/situation/check.svg";
 import Clock from "../../assets/situation/clock.svg";
 import ForkAndKnife from "../../assets/situation/fork-knife.svg";
@@ -10,107 +10,108 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Meal from "../../components/Meal";
 import Wrapper from "../../components/Wrapper";
+import Loading from "../../components/Loading";
+import useRequest from "../../hooks/useRequest";
 
 const Order = () => {
-    const [mealsAdd, setMealsAdd] = useState([
-        {
-            id: 1,
-            title: "Pizza",
-            price: 32.59,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            meal_amount: 2,
-        },
-        {
-            id: 2,
-            title: "Macarrão",
-            price: 32.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            meal_amount: 1,
-        },
-        {
-            id: 5,
-            title: "Macarrão 6",
-            price: 32.05,
-            image:
-                "photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            meal_amount: 3,
-        },
-    ]);
+  const [orderInfos, setOrderInfos] = useState();
+  const navigate = useNavigate();
+  const { manageRequests } = useRequest();
 
-    const [totalPrice, setTotalPrice] = useState(0);
+  const { id } = useParams();
 
-    const [situation, setSituation] = useState("pending");
-
-    function renderSituation(situation) {
-        switch (situation) {
-            case "pending":
-                return (
-                    <>
-                        <img src={Clock} alt="Foto de um relógio." />
-                        <p>Aguardando confirmação do pagamento</p>
-                    </>
-                );
-            case "preparing":
-                return (
-                    <>
-                        <img src={Check} alt="Foto de um sinal de positivo." />
-                        <p>Pagamento aprovado!</p>
-                    </>
-                );
-            case "delivered":
-                return (
-                    <>
-                        <img src={ForkAndKnife} alt="Foto de um garfo e uma faca." />
-                        <p>Pedido entregue!</p>
-                    </>
-                );
-        }
+  function renderSituation(status) {
+    switch (status) {
+      case "pending":
+        return (
+          <>
+            <img src={Clock} alt="Foto de um relógio." />
+            <p>Aguardando confirmação do pagamento</p>
+          </>
+        );
+      case "preparing":
+        return (
+          <>
+            <img src={Check} alt="Foto de um sinal de positivo." />
+            <p>Pagamento aprovado!</p>
+          </>
+        );
+      case "delivered":
+        return (
+          <>
+            <img src={ForkAndKnife} alt="Foto de um garfo e uma faca." />
+            <p>Pedido entregue!</p>
+          </>
+        );
     }
+  }
 
-    useEffect(() => {
-        const calculatedPrice = mealsAdd.reduce((previousValue, currentValue) => {
-            return previousValue + currentValue.price * currentValue.meal_amount;
-        }, 0);
+  useEffect(() => {
+    async function fetchOrderInfos() {
+      const orderResponse = await manageRequests("get", `/orders/${id}`);
 
-        const formattedPrice = calculatedPrice.toFixed(2);
+      const wasTheRequestSuccessfullyMade = orderResponse.data.id;
 
-        setTotalPrice(formattedPrice);
-    }, [mealsAdd]);
+      if (wasTheRequestSuccessfullyMade) {
+        return setOrderInfos(orderResponse.data);
+      }
 
+      if (orderResponse instanceof Error) {
+        alert(
+          "Não foi possível carregar as informações! Por favor tente novamente mais tarde."
+        );
+      }
 
-    return (
-        <Container>
-            <Header />
-            <Wrapper>
-                <Content>
-                    <Cart>
-                        <h1>Carrinho</h1>
-                        {mealsAdd.map(meal => {
-                            const { id, title, price, image, meal_amount } = meal;
-                            return (
-                                <Meal
-                                    key={String(id)}
-                                    id={id}
-                                    title={title}
-                                    price={Number(price * meal_amount).toFixed(2)}
-                                    image={image}
-                                    meal_amount={meal_amount}
-                                />
-                            );
-                        })}
-                        <p className="total-price">Total: R$ {totalPrice}</p>
-                    </Cart>
-                    <Situation>
-                        <h1>Situação</h1>
-                        <div className="infos-situation">{renderSituation(situation)}</div>
-                    </Situation>
-                </Content>
-            </Wrapper>
-            <Footer />
-        </Container>
-    )
-}
+      if (orderResponse.data) {
+        alert(orderResponse.data.message);
+      } else {
+        alert(
+          "Não foi possível carregar as informações! Por favor tente novamente mais tarde."
+        );
+      }
 
-export default Order
+      return navigate("/");
+    }
+    fetchOrderInfos();
+  }, [id, manageRequests, navigate]);
+
+  return (
+    <Container>
+      <Header />
+      <Wrapper>
+        {!orderInfos ? (
+          <Loading />
+        ) : (
+          <Content>
+            <Cart>
+              <h1>Carrinho</h1>
+              {orderInfos.meals.map((meal) => {
+                const { id, title, price, image, meal_amount } = meal;
+                return (
+                  <Meal
+                    key={String(id)}
+                    id={id}
+                    title={title}
+                    price={Number(price * meal_amount).toFixed(2)}
+                    image={image}
+                    meal_amount={meal_amount}
+                  />
+                );
+              })}
+              <p className="total-price">Total: R$ {orderInfos.price}</p>
+            </Cart>
+            <Situation>
+              <h1>Situação</h1>
+              <div className="infos-situation">
+                {renderSituation(orderInfos.status)}
+              </div>
+            </Situation>
+          </Content>
+        )}
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Order;
