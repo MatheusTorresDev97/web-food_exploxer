62c058add4df461894a85d5fc7336f54cd406518
Matheus <matheustorresdev@gmail.com>
2023-12-08 20:22:32 -0300

    fix: solves the bug when deleting a meal

--- src/components/AdmButtons/index.jsx
+++ src/components/AdmButtons/index.jsx
@@ -1,15 +1,25 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 import { FiEdit2, FiTrash } from "react-icons/fi";
 
 import { Container } from "./styles";
-import  Button  from "../Button";
+import Button from "../Button";
+
+import { useAdmButtons } from './useAdmButtons';
 
 const AdmButtons = ({ meal_id }) => {
+  const { handleDeleteMeal, handleEditMeal } = useAdmButtons({ meal_id });
   return (
     <Container>
-      <Button icon={FiEdit2} title="Editar" isHighlighted={false} />
-      <Button icon={FiTrash} title="Excluir" />
+      <Button
+        icon={FiEdit2}
+        title="Editar"
+        isHighlighted={false}
+        onClick={handleEditMeal}
+      />
+      <Button
+        icon={FiTrash}
+        title="Excluir"
+        onClick={handleDeleteMeal}
+      />
     </Container>
   )
 }
